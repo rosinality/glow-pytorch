1a190817353341d80f8cd00372ede0e3b421c0a2
Piotr Tempczyk <piotr.tempczyk@opium.sh>
2020-09-22 12:22:39 +0200

    black on whole repository

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 for d in 0.01 0.01292
 do
-    python test.py --batch 32 --img_size 16 --iter 6 --lr=0.00005 --delta=$d ./checkpoint/model_"$d"_.pt ./mnist/training/
+    python test_mnist.py --batch 32 --img_size 16 --iter 6 --lr=0.00005 --delta=$d ./checkpoint/model_"$d"_.pt ./mnist/training/
 done
 
