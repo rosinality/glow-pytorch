24a2dc98b7d2f3d584b7e0f0ad12a3b7b7b92283
Piotr Tempczyk <piotr.tempczyk@opium.sh>
2020-10-19 09:12:01 +0200

    test on outliers

--- run_test_mnist_on_fashion.sh
+++ run_test_mnist_on_fashion.sh
@@ -1,1 +1,1 @@
-python test.py `cat ./parameters/mnist.params` --device cuda:0 "$1"
+python test.py `cat ./parameters/fashion.params` --device cuda:0 "$1"
---
+++ srun_test_mnist.sh
@@ -0,0 +1,7 @@
+source /home/ptempczyk/virtualenvs/glow-pytorch/bin/activate
+for f in `ls "checkpoint/model_batch#64\;n_channels#1\;epochs#200\;n_flow#32\;n_block#4\;no_lu#False\;affine#True\;n_bits#8\;lr#5e-05\;img_size#32\;temp#0.7\;n_sample#20\;dataset#mnist\;device#cuda\:0\;delta#*100*"`
+do
+    srun --partition=common --qos=8gpu7d --gres=gpu:1 bash run_test_mnist.sh $f &
+    srun --partition=common --qos=8gpu7d --gres=gpu:1 bash run_test_mnist_on_fashion.sh $f &
+done
+deactivate
