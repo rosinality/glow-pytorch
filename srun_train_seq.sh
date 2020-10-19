2919f87208b04bf4382602f7b78427012fead1e2
Piotr Tempczyk <piotr.tempczyk@opium.sh>
2020-10-19 14:47:37 +0200

    normal train on fashion + mnist & fashion sequence

--- srun_train_seq.sh
+++ srun_train_seq.sh
@@ -1,4 +1,4 @@
 source /home/ptempczyk/virtualenvs/glow-pytorch/bin/activate
-python ./train_delta_sequence.py `cat ./parameters/mnist.params` --device cuda:0 &
-python ./train_delta_sequence.py `cat ./parameters/fashion.params` --device cuda:0 &
+srun --partition=common --qos=8gpu7d --gres=gpu:1 python ./train_delta_sequence.py `cat ./parameters/mnist.params` --device cuda:0 &
+srun --partition=common --qos=8gpu7d --gres=gpu:1 python ./train_delta_sequence.py `cat ./parameters/fashion.params` --device cuda:0 &
 deactivate
