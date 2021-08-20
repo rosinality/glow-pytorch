d8423c77c64cbf9830157d332fe2308d1b04117f
Rafał Michaluk <rm360179@students.mimuw.edu.pl>
2021-08-20 10:01:29 +0200

    ll summary with plot

--- ll_results_summary.py
+++ ll_results_summary.py
@@ -42,31 +42,39 @@ def powerset(s):
         yield [ss for mask, ss in zip(masks, s) if i & mask]
 
 
-def summary(ll_path, epochs):
-    results = list()
-    print((('dim', ('inds',) + (('epoch', ),))))
-    for epoch in list(range(10, epochs)):
+def summary(ll_path, epochs, deltas_count, deltas_total):
+    assert 2 <= deltas_count <= deltas_total
+    #results = list()
+    #results = [list() for _ in range(9)]
+    #print((('dim', ('inds',) + (('epoch', ),))))
+    #results = [pd.DataFrame(columns=['epochs', 'avg_delta', 'dim']) for _ in range(deltas+1)]
+    dims = list()
+    #results = [[list() for _ in range(epochs)] for _ in range(deltas_count+1)]
+
+
+    for epoch in list(range(1, epochs+1)):
+        dims.append(list())
         fnames = sorted(glob(os.path.join(ll_path, f"ll_*_{epoch}.txt")))
         points = []
         for f in fnames:
             txt = pd.read_csv(f, sep=" ", header=None)
             noise, pz, logdet, _, _ = txt.mean()
             points.append([noise, -(pz + logdet)])
+        assert deltas_total == len(points)
         points = np.array(points)
+        deltas = list()
+        for first_delta in range(0, deltas_total-deltas_count+1):
+            inds = np.arange(first_delta, first_delta+deltas_count)
+            #print(points)
+            #print(inds)
+            avg_delta = sum(points[ind][0] for ind in inds)/len(inds)
+            dim = calculate_coeff(points[inds, :], log=True, plot=False)
+            dims[epoch-1].append(dim)
+            deltas.append(avg_delta)
 
-        all_deltas_inds = [x for x in powerset(list(range(8))) if len(x) >= 2]
-        for inds in all_deltas_inds:
-            dim = calculate_coeff(points[np.array(inds), :], log=True, plot=False)
-            results.append((dim, tuple(inds) + ((epoch, ),)))
+    return np.array(dims), np.array(deltas), np.arange(1, epochs+1)
 
-    results.sort()
-    for r in results:
-        print(r)
 
-    print('----------')
-    print('deltas:')
-    for i in range(points.shape[0]):
-        print(f"{i}: {points[i][0]}")
 
 #summary(f"/home/rm360179/glow-pytorch/results_ffhq_50/ll", 75)
 #summary(f"/home/rm360179/glow-pytorch/ll", 40)
@@ -74,8 +82,28 @@ def summary(ll_path, epochs):
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", help="number of epochs", required=True)
 parser.add_argument("-ll", help="path to ll", required=True)
+parser.add_argument("-d", help="number of deltas", required=True)
+parser.add_argument("-ds", help="size of deltas subsets", required=True)
 args = parser.parse_args()
 
+deltas_subset_size = 2
 ll_path = args.ll
 max_epochs = int(args.n)
-summary(ll_path, max_epochs)
+deltas_total = int(args.d)
+deltas_subset_size=int(args.ds)
+dims, deltas, epochs = summary(ll_path, max_epochs, deltas_subset_size, deltas_total)
+
+print(deltas)
+print(dims)
+#print(np.vstack((deltas, dims)))
+#plt.yticks(np.array(deltas))
+#plt.xticks(np.arange(deltas_total-deltas_subset_size))
+plt.imshow(dims)
+plt.savefig(f'plots/plot.png')
+
+#plt.imshow(results[i]['dim'].to_numpy().reshape((max_epochs, deltas-i)))
+    #deltas = results[i]['avg_delta'].to_numpy()
+    #plt.xticks(np.arange(deltas-i))
+    #plt.yticks(np.arange(1, max_epochs+1))
+    #plt.imshow(results[i]['epochs'].to_numpy().reshape((max_epochs, deltas-i)), interpolation='bilinear')
+    #print(results[i]['epochs'].to_numpy())
